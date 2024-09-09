34b6f8d70091077233749565de0f80cc1b727114
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-09 12:01:48 +0100

    Updated ColorPicker.jsx

--- src/assets/ColorPicker.jsx
+++ src/assets/ColorPicker.jsx
@@ -3,12 +3,19 @@ import React, { useState } from "react";
 function ColorPicker() {
 
     const [color, setColor] = useState("#ffffff");
+
+    function handleColorChange(e){
+
+    }
+    
 return (
     <div className="color-picker-container">
         <h1>Color Picker</h1>
         <div className="color-display" style={{background: color}}>
             <p>Selected Color: {color}</p>
         </div>
+        <label htmlFor="color">Select Color</label>
+        <input type="color" value={color} onChange={handleColorChange} />
     </div>
 )
 }

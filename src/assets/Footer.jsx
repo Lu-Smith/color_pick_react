3740eab360613d2b267d6cb01034e415cc01ba32
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-09 15:18:35 +0100

    Updated Footer.jsx

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer() {
     return(
         <footer>
-            Coded by <a href="https://www.lunasmithart.com/">Luna Smith</a>
-            , open-source <a href="https://github.com/Lu-Smith/color_pick_react">GitHub</a>.
+            Coded by <a href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a>
+            , open-source <a href="https://github.com/Lu-Smith/color_pick_react" target="_blank">GitHub</a>.
         </footer>
     )
 }

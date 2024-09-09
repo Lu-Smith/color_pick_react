fa14fc35309ea724f64d9af80401f34138722781
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-09 15:07:01 +0100

    Added Footer to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import ColorPicker from './assets/ColorPicker'
+import Footer from './assets/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <ColorPicker />
+     <Footer />
     </>
   )
 }
